fc08fb298ad3b77cd95ee6346d6a0bce2053bb1e
Albers <Albers@DESKTOP-CCDGI9O>
2019-11-03 19:45:24 +0100

    temperatura // hecho

--- src/ClaseTemperatura/Test.java
+++ src/ClaseTemperatura/Test.java
@@ -3,8 +3,14 @@ package ClaseTemperatura;
 public class Test {
 
 	public static void main(String[] args) {
-			Farenheit f = new Temperatura ();
+		Temperatura f = new Temperatura();
+		Temperatura c = new Temperatura();
 
+		double farenheit = 40;
+		double celsius = 29;
+
+		System.out.println("Temperatura de 29º Celsius en Farenheit es " + f.celsiusTofarenheit(celsius));
+		System.out.println(("Temperatura de 40º farenheit en Celsius es ") + c.farenheitTocelsius(farenheit));
 	}
 
 }
