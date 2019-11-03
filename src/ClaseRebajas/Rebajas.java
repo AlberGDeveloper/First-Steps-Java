df9aacbc99ae64228b61564c3573ec568eba7f99
Ionut Alin <Ionut Alin@LAPTOP-JD7FK90J>
2019-11-03 16:08:53 +0100

    Ejercicio Rebajas, método descubrePorcentaje

--- src/ClaseRebajas/Rebajas.java
+++ src/ClaseRebajas/Rebajas.java
@@ -1,5 +1,14 @@
 package ClaseRebajas;
 
-public class Rebajas {
+	public class Rebajas {
+		public static void main(String[] args) {
+			
+		}
+		public static void descubrePorcentaje() {
+			int precioRebajado = 10;
+			int precioOriginal = 20;
+			float Porcentaje = 100 * precioRebajado / precioOriginal;
+			System.out.println(Porcentaje);
+		}	}
 
-}
+
